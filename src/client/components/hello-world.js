1e05ecd6509ce368de1d4450acbf02a6efd65c67
Arnaud-De-Baerdemaeker <de.baerdemaeker.arnaud@gmail.com>
2020-10-01 12:01:23 +0200

    Main component

---
+++ src/client/components/hello-world.js
@@ -0,0 +1,13 @@
+import React from "react";
+import Modal from "./modal";
+
+import "./../css/style.css";
+
+const HelloWorld = () => (
+    <div>
+        <h1>{"Hello world !"}</h1>
+        <Modal />
+    </div>
+);
+
+export default HelloWorld;
