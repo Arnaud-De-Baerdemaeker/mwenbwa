6e2f69d56d1736323d2fc90126977a9caacd92d4
Arnaud-De-Baerdemaeker <de.baerdemaeker.arnaud@gmail.com>
2020-10-01 12:00:22 +0200

    Root of the app

---
+++ src/client/app.js
@@ -0,0 +1,13 @@
+/* becodeorg/mwenbwa
+ *
+ * /src/client/app.js - Client entry point
+ *
+ * coded by leny@BeCode
+ * started at 18/05/2020
+ */
+
+import * as React from "react";
+import ReactDOM from "react-dom";
+import HelloWorld from "./components/hello-world";
+
+ReactDOM.render(<HelloWorld />, document.querySelector("#app"));
