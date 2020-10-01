df4e361c6c2e99c50bb035f3aeb3e940331c666d
Arnaud-De-Baerdemaeker <de.baerdemaeker.arnaud@gmail.com>
2020-10-01 12:03:55 +0200

    Build the submit component

---
+++ src/client/components/modal-components/submit.js
@@ -0,0 +1,16 @@
+// Project Mwenbwa
+// src/client/components/submit.js - Submit component
+// coded by arnaud-de-baerdemaeker-jepsen-3.20@becode
+
+import React from "react";
+
+const Submit = () => (
+    <div className={"submit"}>
+        {/*eslint-disable-next-line react/button-has-type*/}
+        <button type={"submit"} className={"submit__button"}>
+            {"Submit"}
+        </button>
+    </div>
+);
+
+export default Submit;
