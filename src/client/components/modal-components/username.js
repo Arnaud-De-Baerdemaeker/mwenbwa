9dbd9225f8d28e54fefd759376b54d242fbebf1f
Arnaud-De-Baerdemaeker <de.baerdemaeker.arnaud@gmail.com>
2020-10-02 16:55:43 +0200

    Add the state and function to handle changes

--- src/client/components/modal-components/username.js
+++ src/client/components/modal-components/username.js
@@ -2,20 +2,26 @@
 // src/client/components/username.js - Username component
 // coded by arnaud-de-baerdemaeker-jepsen-3.20@becode
 
-import React from "react";
+import React, {useState} from "react";
 
-const Username = () => (
-    <div className={"username"}>
-        <label htmlFor={"username"} className={"username__label"}>
-            {"Username"}
-        </label>
-        <input
-            type={"text"}
-            name={"username"}
-            className={"username__field"}
-            required
-        />
-    </div>
-);
+const Username = () => {
+    const [username, setUsername] = useState("");
+
+    return (
+        <div className={"username"}>
+            <label htmlFor={"username"} className={"username__label"}>
+                {"Username"}
+            </label>
+            <input
+                type={"text"}
+                name={"username"}
+                value={username}
+                onChange={error => setUsername(error.target.value)}
+                className={"username__field"}
+                required
+            />
+        </div>
+    );
+};
 
 export default Username;
