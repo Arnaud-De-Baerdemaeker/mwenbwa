7a1c49fc5f4f746e7a31e3cfd3e9c367aaa82cc4
Arnaud-De-Baerdemaeker <de.baerdemaeker.arnaud@gmail.com>
2020-10-02 16:53:54 +0200

    Add the state and the function to handle changes

--- src/client/components/modal-components/password.js
+++ src/client/components/modal-components/password.js
@@ -2,15 +2,26 @@
 // src/client/components/password.js - Password component
 // coded by arnaud-de-baerdemaeker-jepsen-3.20@becode
 
-import React from "react";
+import React, {useState} from "react";
 
-const Password = () => (
-    <div className={"password"}>
-        <label htmlFor={"password"} className={"password__label"}>
-            {"Password (min. 8 characters)"}
-        </label>
-        <input type={"password"} name={"password"} minLength={8} required />
-    </div>
-);
+const Password = () => {
+    const [password, setPassword] = useState("");
+
+    return (
+        <div className={"password"}>
+            <label htmlFor={"password"} className={"password__label"}>
+                {"Password (min. 8 characters)"}
+            </label>
+            <input
+                type={"password"}
+                name={"password"}
+                minLength={8}
+                value={password}
+                onChange={e => setPassword(e.target.value)}
+                required
+            />
+        </div>
+    );
+};
 
 export default Password;
