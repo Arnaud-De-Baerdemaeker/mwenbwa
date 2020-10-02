33d0b97ac0b787b72b72c7347e193b6f835edae4
Arnaud-De-Baerdemaeker <de.baerdemaeker.arnaud@gmail.com>
2020-10-02 16:52:58 +0200

    Add the state and function to handle the change

--- src/client/components/modal-components/email.js
+++ src/client/components/modal-components/email.js
@@ -2,15 +2,25 @@
 // src/client/components/email.js - Email component
 // coded by arnaud-de-baerdemaeker-jepsen-3.20@becode
 
-import React from "react";
+import React, {useState} from "react";
 
-const Email = () => (
-    <div className={"email"}>
-        <label htmlFor={"email"} className={"email__label"}>
-            {"E-mail"}
-        </label>
-        <input type={"email"} name={"E-mail"} required />
-    </div>
-);
+const Email = () => {
+    const [email, setEmail] = useState("");
+
+    return (
+        <div className={"email"}>
+            <label htmlFor={"email"} className={"email__label"}>
+                {"E-mail"}
+            </label>
+            <input
+                type={"email"}
+                name={"E-mail"}
+                value={email}
+                onChange={e => setEmail(e.target.value)}
+                required
+            />
+        </div>
+    );
+};
 
 export default Email;
