c6b9e32e1b619fa0527bdc28c10004cd9ca34613
Arnaud-De-Baerdemaeker <de.baerdemaeker.arnaud@gmail.com>
2020-10-02 17:00:15 +0200

    Add the state, function to handle the changes and begin password hashing

--- src/client/components/modal-components/sign-in.js
+++ src/client/components/modal-components/sign-in.js
@@ -6,27 +6,42 @@
  * started at 09/2020
  */
 
-import React from "react";
+import React, {useState} from "react";
 import {CirclePicker} from "react-color";
+// import bcryptjs from "bcryptjs";
 import Username from "./username";
 import Email from "./email";
 import Password from "./password";
 import Submit from "./submit";
 
-const SignIn = () => (
-    <form className={"sign-in"}>
-        <h2 className={"sign-in__title"}>{"Create an account"}</h2>
-        <Username />
-        <Email />
-        <Password />
-        <div className={"color"}>
-            <h3 className={"color__title"}>{"Pick a color"}</h3>
-            <div className={"color__container"}>
-                <CirclePicker />
+const SignIn = () => {
+    const [, /*color*/ setColor] = useState("");
+
+    const changeSpy = props => {
+        setColor(props.hex);
+    };
+
+    const submit = event => {
+        event.preventDefault();
+
+        // bcryptjs.hash(Password.password, 10).then(result => {})
+    };
+
+    return (
+        <form onSubmit={submit} className={"sign-in"}>
+            <h2 className={"sign-in__title"}>{"Create an account"}</h2>
+            <Username />
+            <Email />
+            <Password />
+            <div className={"color"}>
+                <h3 className={"color__title"}>{"Pick a color"}</h3>
+                <div className={"color__container"}>
+                    <CirclePicker onChange={changeSpy} />
+                </div>
             </div>
-        </div>
-        <Submit />
-    </form>
-);
+            <Submit />
+        </form>
+    );
+};
 
 export default SignIn;
