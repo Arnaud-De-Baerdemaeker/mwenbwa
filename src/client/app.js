1bdf777076219ec7e32b811c61efd6894b8828cb
AhmedSoleiman <ahmed.soleiman.mad@gmail.com>
2020-10-06 12:30:30 +0200

    Add react markercluster

--- src/client/app.js
+++ src/client/app.js
@@ -4,12 +4,12 @@
  *
  * coded by leny@BeCode
  * started at 18/05/2020
- * 
+ *
  */
 
 import * as React from "react";
 import ReactDOM from "react-dom";
 import Game from "./components/Game";
-import "leaflet/dist/leaflet.css"
+import "leaflet/dist/leaflet.css";
 
 ReactDOM.render(<Game />, document.querySelector("#app"));
