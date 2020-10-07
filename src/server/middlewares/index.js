e56298d2dd8a9584af2e0da62fb71f18a88421fb
Arnaud-De-Baerdemaeker <de.baerdemaeker.arnaud@gmail.com>
2020-10-07 10:48:50 +0200

    [del] Delete
    Delete server files to start over

--- src/server/middlewares/index.js
+++
@@ -1,9 +0,0 @@
-import authJwt from "./auth-jwt";
-import verifySignUp from "./verify-sign-up";
-// const authJwt = require("./auth-jwt");
-// const verifySignUp = require("./verify-sign-up");
-
-module.exports = {
-    authJwt,
-    verifySignUp,
-};
