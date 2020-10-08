d80d6053114a02b22aa1ed91571385ae77090e00
Arnaud-De-Baerdemaeker <de.baerdemaeker.arnaud@gmail.com>
2020-10-08 09:38:43 +0200

    [add] Add constants to control the modal content
    - Created 2 constants to control the states of the sign-in and log-in content.
    - Passing the props to the SignIn and LogIn components.

--- src/client/components/modal.js
+++ src/client/components/modal.js
@@ -2,18 +2,32 @@
 // src/client/components/modal.js - Modal component
 // coded by arnaud-de-baerdemaeker-jepsen-3.20@becode
 
-import React from "react";
+import React, {useState} from "react";
 import {createPortal} from "react-dom";
 import SignIn from "./modal-components/sign-in";
 import LogIn from "./modal-components/log-in";
 
-const Modal = () =>
-    createPortal(
+const Modal = () => {
+    const [signInIsDisplayed, setSignInIsDisplayed] = useState(true);
+    const [logInIsDisplayed, setLogInIsDisplayed] = useState(false);
+
+    return createPortal(
         <div className={"modal"}>
-            <SignIn />
-            <LogIn />
+            <SignIn
+                signInIsDisplayed={signInIsDisplayed}
+                setSignInIsDisplayed={setSignInIsDisplayed}
+                logInIsDisplayed={logInIsDisplayed}
+                setLogInIsDisplayed={setLogInIsDisplayed}
+            />
+            <LogIn
+                logInIsDisplayed={logInIsDisplayed}
+                setLogInIsDisplayed={setLogInIsDisplayed}
+                signInIsDisplayed={signInIsDisplayed}
+                setSignInIsDisplayed={setSignInIsDisplayed}
+            />
         </div>,
         document.querySelector("#modal"),
     );
+};
 
 export default Modal;
