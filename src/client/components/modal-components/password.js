796eed54e35220e88fc84d820ed8bbdee41d0e43
Arnaud-De-Baerdemaeker <de.baerdemaeker.arnaud@gmail.com>
2020-10-09 15:11:27 +0200

    Delete elements which have no more use
    Deleted the function which updated the state of the object variable.
    Deleted the value attribute.
    Deleted the onChange attribute.

--- src/client/components/modal-components/password.js
+++ src/client/components/modal-components/password.js
@@ -4,26 +4,12 @@
 
 import React from "react";
 
-const Password = props => {
-    function handleChange(e) {
-        props.setNewUser({...props.newUser, password: e.target.value});
-    }
-
-    return (
-        <div className={"password"}>
-            <label htmlFor={"password"} className={"password__label"}>
-                {"Password (min. 8 characters)"}
-            </label>
-            <input
-                type={"password"}
-                name={"password"}
-                minLength={8}
-                // value={props.newUser.password}
-                onChange={handleChange}
-                required
-            />
-        </div>
-    );
-};
-
+const Password = () => (
+    <div className={"password"}>
+        <label htmlFor={"password"} className={"password__label"}>
+            {"Password (min. 8 characters)"}
+        </label>
+        <input type={"password"} name={"password"} minLength={8} required />
+    </div>
+);
 export default Password;
