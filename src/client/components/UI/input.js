47462a672765b4a819d601a008a155c30e1dbfcb
Arnaud-De-Baerdemaeker <de.baerdemaeker.arnaud@gmail.com>
2020-10-14 10:45:28 +0200

    Fix syntax

---
+++ src/client/components/UI/input.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+const Input = props => {
+    const {label, ...attr} = props;
+
+    return (
+        <div>
+            <label>{props.label}</label>
+            <input {...attr} />
+        </div>
+    );
+};
+
+export default Input;
