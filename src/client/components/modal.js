3464f00600fab0b760b4ef1d0e62bf328a7d5f26
Arnaud-De-Baerdemaeker <de.baerdemaeker.arnaud@gmail.com>
2020-10-14 10:48:53 +0200

    Add controls

--- src/client/components/modal.js
+++ src/client/components/modal.js
@@ -2,32 +2,33 @@
 // src/client/components/modal.js - Modal component
 // coded by arnaud-de-baerdemaeker-jepsen-3.20@becode
 
-import React, {useState} from "react";
+import React from "react";
 import {createPortal} from "react-dom";
 import SignIn from "./modal-components/sign-in";
 import LogIn from "./modal-components/log-in";
 
-const Modal = () => {
-    const [signInIsDisplayed, setSignInIsDisplayed] = useState(true);
-    const [logInIsDisplayed, setLogInIsDisplayed] = useState(false);
-
-    return createPortal(
-        <div className={"modal"}>
+const Modal = ({
+    modalIsDisplayed,
+    // setModalIsDisplayed,
+    signInIsDisplayed,
+    setSignInIsDisplayed,
+    logInIsDisplayed,
+    setLogInIsDisplayed,
+}) =>
+    createPortal(
+        <div className={modalIsDisplayed ? "modal--open" : "modal--closed"}>
             <SignIn
                 signInIsDisplayed={signInIsDisplayed}
                 setSignInIsDisplayed={setSignInIsDisplayed}
-                logInIsDisplayed={logInIsDisplayed}
                 setLogInIsDisplayed={setLogInIsDisplayed}
             />
             <LogIn
                 logInIsDisplayed={logInIsDisplayed}
                 setLogInIsDisplayed={setLogInIsDisplayed}
-                signInIsDisplayed={signInIsDisplayed}
                 setSignInIsDisplayed={setSignInIsDisplayed}
             />
         </div>,
         document.querySelector("#modal"),
     );
-};
 
 export default Modal;
